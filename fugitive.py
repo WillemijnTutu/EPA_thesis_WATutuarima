85197fc07e69c8cf68bc0f3a8f7de0dd22c42942
WillemijnTutu <twillemijn@gmail.com>
2023-04-24 10:34:59 +0200

    documentation fugitive file

--- fugitive.py
+++ fugitive.py
@@ -1,15 +1,30 @@
-import numpy as np
 from pydsol.model.entities import Entity
 import itertools
 
 
 class Fugitive(Entity):
+    """
+    Class used to simulate a fugitive entity
+
+    Parameters
+    ----------
+    simulator:
+        PyDSOL core simulator
+    t:int
+        time_creations
+    kwargs:
+        the keyword arguments that are used to expand the entity class
+    """
 
     id_iter_fug = itertools.count(1)
 
     def __init__(self, simulator, t, **kwargs):
-        # self.speed = np.random.triangular(0, 30, 50)  # km/h to day
-        self.speed = 500 #30 km/h is 500 m per minuut
+        """
+        Method to initialise fugitive entity
+        """
+
+        self.speed = 500
+        # 30 km/h is 500 m per minuut
         super().__init__(simulator, t, self.speed, **kwargs)
 
         self.name = f"{self.__class__.__name__} {str(next(self.id_iter_fug))}"
